f4d43072af21be64e57ecf1c93f02831315ce7aa
Michel Banckaert <banckaertm@hotmail.com>
2024-08-15 08:16:38 +0200

    [EDIT] webpack.common.js modif

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports =
         module: {
             rules: [
                 {
-                    test: /\.(js|mjs|ts)$/,
+                    test: /\.(js|cjs|mjs|ts)$/,
                     exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
